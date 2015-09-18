2d043104ba01eab640496e40fd3a5b28ec8b9801
ana paula <anapnascimento86@gmail.com>
2015-09-18 14:15:58 -0300

    programa para organizar os numeros da lista em ordem crescente

---
+++ ana-leticia-angelica/bubble-sort.py
@@ -0,0 +1,13 @@
+lista = [11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8]
+N = 20 
+#chamamos de variavel i a posição do elemento na lista criada
+for i in range(0, N - 1, 1): 
+#chamamos de variavel j a posição imendiatamente após i,ou seja, i + 1
+    for j in range(i + 1, N, 1): 
+#se elemento na posição i for maior que o da posicao j, troca-se de posição. Se não, mantem.
+        if lista[i] > lista[j]: 
+            temp = lista[i]
+            lista[i] = lista[j]
+            lista[j] = temp
+#para mostrar a lista modificada.
+print(lista) 
