296efea711a212fab02f2e82ce47b3fec915e816
leticia alvarenga <le.marinho@yahoo.com.br>
2015-09-25 08:46:37 -0300

    lista com linhas original e ordem crescente

--- ana-leticia-angelica/bubble-sort.py
+++ ana-leticia-angelica/bubble-sort.py
@@ -1,5 +1,7 @@
 lista = [11, 18, 3, 1, 16, 12, 6, 19, 5, 0, 14, 4, 17, 9, 13, 7, 10, 15, 2, 8]
 N = 20 
+#Imprimindo a lista original
+print("Lista original", lista)
 #chamamos de variavel i a posição do elemento na lista criada
 for i in range(0, N - 1, 1): 
 #chamamos de variavel j a posição imendiatamente após i,ou seja, i + 1
@@ -9,5 +11,5 @@ for i in range(0, N - 1, 1):
             temp = lista[i]
             lista[i] = lista[j]
             lista[j] = temp
-#para mostrar a lista modificada.
-print(lista) 
+#para mostrar a lista modificada em ordem crescente.
+print("Lista em ordem crescente", lista) 
